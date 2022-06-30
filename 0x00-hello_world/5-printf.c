1b2800663b856faa663a9996b1896745c35e2db5
Joshua <joshuadami17@gmail.com>
2022-06-30 04:24:00 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/**
+ * header func
+ */
+#include <stdio.h>
+/**
+ * main func
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	/**
+	 * return func
+	 */
+	return (0);
+}
