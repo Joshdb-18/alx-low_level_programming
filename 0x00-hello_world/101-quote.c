3de430f82d537855b7bdaa16fd0d1a2ed0dc6504
Joshua <joshuadami17@gmail.com>
2022-06-30 08:12:50 -0700

    Print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - main func
+ * main - complete func
  * Description: print a failure value
- * RETURN: 1
+ * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
