4324a45c3339148902a6a7a84e74f6f906d467d4
Joshua <joshuadami17@gmail.com>
2022-06-30 08:01:38 -0700

    Print a failure value to the compiler

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main func
+ * Description: print a failure value
+ * RETURN: 1
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
