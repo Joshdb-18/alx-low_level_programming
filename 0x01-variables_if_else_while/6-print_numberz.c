691be352f4d6fbbe1b629e1c38898884f2811d17
Joshua <joshuadami17@gmail.com>
2022-07-01 09:15:07 -0700

    push

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main func
+ * Description: desc
+ * Return: 0
+ */
+int main(void)
+{
+	long long n = 0123456789;
+	int p = '\n';
+
+	while (n != 0)
+		putchar((n % 10) + '0');
+		n = n / 10;
+	putchar(p);
