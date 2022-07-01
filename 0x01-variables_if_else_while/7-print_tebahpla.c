03b0c104dae53307639ff49e67a95ea897b62f4f
Joshua <joshuadami17@gmail.com>
2022-07-01 09:42:31 -0700

    Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main func
+ * Description: func
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+	char p = '\n';
+
+	for (c = 'z'; c >= 'a'; c++)
+		putchar(c);
+	putchar(p);
+
+	return (0);
+}
