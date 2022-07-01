3a637ef365714ef82c52ee226c03d971e086e816
Joshua <joshuadami17@gmail.com>
2022-07-01 10:08:22 -0700

    final

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,16 @@ int main(void)
 {
 	int n = 0;
 	char c[2] =", ";
-	char n = '\n';
+	char ne = '\n';
 	char d = '$';
-
+	
 	for (n = 0; n <= 9; n++)
+	{
 		putchar(n + '0');
 		putchar(c);
+	}
 	putchar(d);
-	putchar(n);
+	putchar(ne);
 
 	return (0);
 }
