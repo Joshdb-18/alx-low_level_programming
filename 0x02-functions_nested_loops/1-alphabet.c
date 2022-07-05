e88930f81ca8320b8b06c3b7582c58ea01f6b687
Joshua <joshuadami17@gmail.com>
2022-07-05 05:38:39 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /** main - check code
  * Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(char*);
 
 int main(void)
 {
@@ -15,7 +15,7 @@ int main(void)
  * print_alphabet - print alphabets
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet(char*)
 {
 	char n = 'a';
 
