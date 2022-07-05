66abaf25f0cd683951ca1564ed439769a79827ce
Joshua <joshuadami17@gmail.com>
2022-07-05 14:06:28 -0700

    wert

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,24 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 #include <ctype.h>
 /**
- * main - islower
+ * _islower - islower func
  * Return: 0 or 1
  */
-int _islower(int c);
-
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-
-	return (0);
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
