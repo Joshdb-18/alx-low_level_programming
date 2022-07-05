1b825dd0b79badceaaf269bda84ff41d3fd4df43
Joshua <joshuadami17@gmail.com>
2022-07-05 08:23:46 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,24 @@
 /** main - check code
  * Return: 0
  */
-void print_alphabet(void);
 
 int main(void)
 {
 	print_alphabet();
 
 	return (0);
+}
+/**
+ * print_alphabet - alpha
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
