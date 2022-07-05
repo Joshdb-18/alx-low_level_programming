772e6fc74cf40aa8abdfa52f5999d25670f3ab59
Joshua <joshuadami17@gmail.com>
2022-07-05 12:54:29 -0700

    m

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -27,3 +27,18 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int num = 10;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
