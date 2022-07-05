02242681ffb9b71e5ff2f9261f0c023b05b218d3
Joshua <joshuadami17@gmail.com>
2022-07-05 03:01:30 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * main - main func
+ * _isalpha - caps
+ * Return: 0
+ */
+int _isalpha(int c);
+
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
+
+int _isalpha(int c)
+{
+	if (isalpha(c) || islower(c) || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
