9aa0b933dac372ce1293550e30912cf125306d1a
Joshua <joshuadami17@gmail.com>
2022-07-05 09:00:30 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,16 +9,3 @@ int main(void)
 
 	return (0);
 }
-/**
- * print_alphabet - alpha
- */
-void print_alphabet(void)
-{
-	char c = 'a';
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -16,3 +16,14 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 
 }
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
