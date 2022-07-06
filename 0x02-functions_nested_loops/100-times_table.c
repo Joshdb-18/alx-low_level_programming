3775dd09b57e49baa7ce7d20306e4f388a8bf22b
Joshua <joshuadami17@gmail.com>
2022-07-05 18:16:30 -0700

    fibonacci

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,22 +6,40 @@
  */
 void print_tiimes_table(int n)
 {
-	if (n > 15 || n < 0)
-	{
-		return;
-	}
-	else
-	{
-		int count = 0;
-		int next;
+	int num, mult, prod;
 
-		for (count = 0; count <= n; count++)
+	if (n >= 0 || n <= 15)
+	{
+		for(num = 0; num <= n; num++)
 		{
-			for (next = 0; next <= n; next++)
+			_putchar('0');
+
+			for (mult = 0; mult <= n; mult++)
 			{
-				_putchar(next);
 				_putchar(',');
 				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+				{
+					_putchar(' ');
+				}
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 10) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
