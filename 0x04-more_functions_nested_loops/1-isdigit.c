64200fdc2954c9c519cd244d20b0c7abedab7ab5
Joshua <joshuadami17@gmail.com>
2022-07-07 01:51:00 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - check for numbers
+ *
+ * @c: parameter to print
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if ((isdigit(c)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
