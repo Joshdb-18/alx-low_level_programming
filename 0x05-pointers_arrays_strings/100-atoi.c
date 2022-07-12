0734bd2690668f89aea133e5f9641579189fa66d
Joshua <joshuadami17@gmail.com>
2022-07-12 19:39:42 +0000

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,11 +22,11 @@ int _atoi(char *s)
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
-			num = (num * 10) + (*s - '0')
+			num = (num * 10) + (*s - '0');
 		}
 		else if (num > 0)
 			break;
-	} while (*s++)
+	} while (*s++);
 	
 	return (num * sign);
 }
