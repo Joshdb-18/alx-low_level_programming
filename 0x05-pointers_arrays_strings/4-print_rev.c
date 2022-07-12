e5a008b9fe1774443004c48586ad85c1bfc833d1
Joshua <joshuadami17@gmail.com>
2022-07-12 11:10:35 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: function para
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i > -1; i++)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
