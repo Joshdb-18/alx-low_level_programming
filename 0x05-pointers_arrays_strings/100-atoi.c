6a3bf93d6c6fb11a6aceb307ab49783011f537a5
Joshua <joshuadami17@gmail.com>
2022-07-12 18:46:54 +0000

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s: func para
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int number = atoi(s);
+
+	return (number);
+}
