4dc641dea55ec50d41fe17ceb84b7c34f7fc8347
Joshua <joshuadami17@gmail.com>
2022-07-14 13:06:51 +0000

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -6,8 +6,9 @@
  *                the byte position, then showing the hex content,
  *                then displaying printable charcaters.
  * @b: The buffer to be printed.
- * @size: The number of bytes to be printed from the buffer.
+ * @size: Number of bytes to be printed from buffer.
  */
+
 void print_buffer(char *b, int size)
 {
 	int byte, index;
