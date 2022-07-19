85f85eae6d925227bcd3a83ccc8f4aff36d57654
Joshua <joshuadami17@gmail.com>
2022-07-19 17:40:05 +0000

    advanced ttasks

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,7 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int reverse(int n);
+int _strlen_recursion(char *s);
+int comparator(char *s, int n1, int n2);
 
 #endif
