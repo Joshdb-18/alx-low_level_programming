d39d1568b07ad9dabad47ddaefe5f5f48b457cec
Joshua <joshuadami17@gmail.com>
2022-07-25 11:08:38 +0000

    If you even dream of beating me you'd bettr wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,22 +6,24 @@
  * @height: heignt
  * Return: NULL or pointer to 2 dim arr
  */
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
 {
-	int *w = 0;
+	int **p;
 
+	p[width][height];
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			_putchar(w);
-		}
-		_putchar('\n');
-	}
-	return (w);
+	/**
+	 * for (i = 0; i < height; i++)
+	 * {
+	 *	for (j = 0; j < width; j++)
+	 *	{
+	 *		_putchar(w);
+	 *	}
+	 *	_putchar('\n');
+	 * }
+	 */
+	return (p);
 }
-
