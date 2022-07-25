99e26cef5a7aa088562b1c12926efbf164cbd7b9
Joshua <joshuadami17@gmail.com>
2022-07-25 10:53:44 +0000

    If you even dream of beating me you'd bettr wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of ints
+ * @width: width
+ * @height: heignt
+ * Return: NULL or pointer to 2 dim arr
+ */
+int **alloc_grid(int width, int height);
+{
+	int *w = 0;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			_putchar(w);
+		}
+		_putchar('\n');
+	}
+	return (w);
+}
+
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - print char to stdout
+ * @c: character to print
+ * Return: 0 on success
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
