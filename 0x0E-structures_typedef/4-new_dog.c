71177755ff50a1d5ef67c27b40980f93b3f4a5d7
Joshua <joshuadami17@gmail.com>
2022-08-01 18:45:33 +0000

    typedef struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,6 +18,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (d == NULL || name == NULL || age < 0 || owner == NULL)
 		return (NULL);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
