abec44d10c19bbaad1daddd1a4ff3de847e4ad9b
Joshua <joshuadami17@gmail.com>
2022-08-01 18:29:42 +0000

    type def struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(dog_t));
 
-	if (d == NULL)
+	if (d == NULL || name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	d->name = name;
 	d->age = age;
