196e449122a640a2c164e650fef8ae768ce8ab8a
Joshua <joshuadami17@gmail.com>
2022-08-03 15:39:56 +0000

    functions pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,14 +8,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, s;
+	int i;
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
-		s = cmp(array[i]);
-	if (s <= 0)
-		return (-1);
-
-	return (s);
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
