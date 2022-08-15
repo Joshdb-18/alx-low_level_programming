124225f07031c0b5c4a15bebbcc2ceffe21d391e
Joshua <joshuadami17@gmail.com>
2022-08-15 17:27:43 +0000

    More singly linked list hot fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,8 +8,12 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new, *last;
 
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 
@@ -25,6 +29,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		last->next = new;
 	}
-	return (*head);
+	return (new);
 }
 
