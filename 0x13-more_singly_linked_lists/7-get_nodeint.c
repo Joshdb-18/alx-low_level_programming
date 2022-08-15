3344c797d943d01b6d9ec142489811b91d6ce1af
Joshua <joshuadami17@gmail.com>
2022-08-15 13:44:53 +0000

    More singly linked list hot fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,6 +2,7 @@
 /**
  * get_nodeint_at_index - gets a node of a linked list
  * @head: pointer to list
+ * @index: index
  * Return: located node or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
