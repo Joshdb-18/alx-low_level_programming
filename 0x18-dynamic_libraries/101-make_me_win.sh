b68e0ee282d917a7d3bb6d4375dae4a73c60f1b4
Joshua <joshuadami17@gmail.com>
2022-10-03 14:14:51 +0000

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Joshdb-18/alx-low_level_programming/raw/master/0x18-dynamic_libraries/preload.so
+wget -q -O /tmp/preload.so https://github.com/Joshdb-18/alx-low_level_programming/raw/master/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
